39236695e1b0d7e0a5d5eb6d89d253a0827c8eff
anhthanh260599@gmail.com <anhthanh260599@gmail.com>
2023-08-26 18:30:47 +0700

    Upcode Giao diện Trang chủ người dùng, thêm class Common để format giá tiền.

---
+++ WebBanHangOnline/WebBanHangOnline/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Partial_ItemsByCateId()
+        {
+            var item = db.Products.Where(x=>x.IsHome && x.IsActive).Take(10).ToList();
+            return PartialView(item);
+        }
+
+        public ActionResult Partial_ProductSale()
+        {
+            var item = db.Products.Where(x=>x.IsSale && x.IsActive).ToList();
+            return PartialView(item);
+        }
+    }
+}
