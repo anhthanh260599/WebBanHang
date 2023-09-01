8c834181c9a50be30fc62b8ab6fd26dc6f9d3b51
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2023-09-01 17:21:28 +0700

    Stash SettingSystem

---
+++ WebBanHangOnline/WebBanHangOnline/Models/SettingSystemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models
+{
+    public class SettingSystemViewModel
+    {
+        // Setting cái gì thì thêm fields đó, ví dụ setting slider hoặc banner thì thêm fields vô.
+        public string SettingTitle { get; set; }
+        public string SettingLogo { get; set; }
+        public string SettingHotline { get; set; }
+        public string SettingEmail { get; set; }
+        public string SettingTitleSeo { get; set; }
+        public string SettingDesSeo { get; set; }
+        public string SettingKeySeo { get; set; }
+
+
+    }
+}
