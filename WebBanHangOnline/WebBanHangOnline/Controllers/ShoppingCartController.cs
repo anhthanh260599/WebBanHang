34dcbe86d859960f6ea2e3f6d5b3f100b2500ed1
Nguyễn Anh Thành <91647866+anhthanh260599@users.noreply.github.com>
2023-09-02 20:38:23 +0700

    Revert "50% VNPAY"

--- WebBanHangOnline/WebBanHangOnline/Controllers/ShoppingCartController.cs
+++ WebBanHangOnline/WebBanHangOnline/Controllers/ShoppingCartController.cs
@@ -8,7 +8,6 @@ using WebBanHangOnline.Models.Common;
 using WebBanHangOnline.Models.EF;
 using System.IO;
 using System.Configuration;
-using WebBanHangOnline.Models.Payment;
 
 namespace WebBanHangOnline.Controllers
 {
@@ -23,13 +22,6 @@ namespace WebBanHangOnline.Controllers
             {
                 ViewBag.CheckCart = cart;
             }
-            return View();
-        }
-
-        public ActionResult VnPayReturn()
-        {
-            //var a = UrlPayment(0, "DH02092300008");
-
             return View();
         }
 
@@ -57,8 +49,8 @@ namespace WebBanHangOnline.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Checkout(OrderViewModel request)
         {
-            var code = new { Success = false, Code = -1, Url = "" };
-            if (ModelState.IsValid)
+            var code = new { success = false, Code = -1 };
+            if(ModelState.IsValid)
             {
                 ShoppingCart cart = (ShoppingCart)Session["Cart"];
                 if (cart != null)
@@ -157,20 +149,15 @@ namespace WebBanHangOnline.Controllers
                     contentAdmin = contentAdmin.Replace("{{ThanhTien}}", Common.FormatNumber(thanhTien, 0));
                     contentAdmin = contentAdmin.Replace("{{TongTien}}", Common.FormatNumber(tongTien, 0));
                     Common.SendMail("Cà phê Nhóm 4", "Đơn hàng mới #" + order.Code, contentAdmin.ToString(), ConfigurationManager.AppSettings["Email"]);
-                    cart.ClearCart();
+
                     //// End Mail ////
-                    code = new { Success = true, Code = request.TypePayment, Url = "" };
-                    //var url =     
-                    if (request.TypePayment == 2)
-                    {
-                        var url = UrlPayment(request.TypePaymentVN, order.Code);
-                        code = new { Success = true, Code = request.TypePayment, Url = url };
-                    }   
-                    //code = new {success = true, Code = 1, Url= Url };
-                    //return RedirectToAction("CheckoutSuccess");
+
+
+                    cart.ClearCart();
+                    return RedirectToAction("CheckoutSuccess");
                 }
             }
-            return Json(code);
+            return Json(request);
         }
 
         public ActionResult Partial_Item_ThanhToan()
@@ -289,68 +276,6 @@ namespace WebBanHangOnline.Controllers
             }
             return Json(new { success = false });
 
-        }
-
-
-        /// <summary>
-        /// Thanh toán VNPAY
-        /// </summary>
-        public string UrlPayment(int TypePaymentVN, string orderCode)
-        {
-            var urlPayment = "";
-            var order = db.Orders.FirstOrDefault(x=>x.Code == orderCode);
-
-            //Get Config Info
-            string vnp_Returnurl = ConfigurationManager.AppSettings["vnp_Returnurl"]; //URL nhan ket qua tra ve 
-            string vnp_Url = ConfigurationManager.AppSettings["vnp_Url"]; //URL thanh toan cua VNPAY 
-            string vnp_TmnCode = ConfigurationManager.AppSettings["vnp_TmnCode"]; //Ma định danh merchant kết nối (Terminal Id)
-            string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"]; //Secret Key
-
-            ////Get payment input
-            //OrderInfo order = new OrderInfo();
-            //order.OrderId = DateTime.Now.Ticks; // Giả lập mã giao dịch hệ thống merchant gửi sang VNPAY
-            //order.Amount = 100000; // Giả lập số tiền thanh toán hệ thống merchant gửi sang VNPAY 100,000 VND
-            //order.Status = "0"; //0: Trạng thái thanh toán "chờ thanh toán" hoặc "Pending" khởi tạo giao dịch chưa có IPN
-            //order.CreatedDate = DateTime.Now;
-            //Save order to db
-
-            //Build URL for VNPAY
-            VnPayLibrary vnpay = new VnPayLibrary();
-            var Price = (long)order.TotalAmount * 100; // Parse lại để bỏ 2 số 00 sau dấu "." thì mới hoạt động được
-            vnpay.AddRequestData("vnp_Version", VnPayLibrary.VERSION);
-            vnpay.AddRequestData("vnp_Command", "pay");
-            vnpay.AddRequestData("vnp_TmnCode", vnp_TmnCode);
-            vnpay.AddRequestData("vnp_Amount", Price.ToString()); //Số tiền thanh toán. Số tiền không mang các ký tự phân tách thập phân, phần nghìn, ký tự tiền tệ. Để gửi số tiền thanh toán là 100,000 VND (một trăm nghìn VNĐ) thì merchant cần nhân thêm 100 lần (khử phần thập phân), sau đó gửi sang VNPAY là: 10000000
-            if (TypePaymentVN == 1)
-            {
-                vnpay.AddRequestData("vnp_BankCode", "VNPAYQR");
-            }
-            else if (TypePaymentVN == 2)
-            {
-                vnpay.AddRequestData("vnp_BankCode", "VNBANK");
-            }
-            else if (TypePaymentVN == 3)
-            {
-                vnpay.AddRequestData("vnp_BankCode", "INTCARD");
-            }
-
-            vnpay.AddRequestData("vnp_CreateDate", order.CreateDate.ToString("yyyyMMddHHmmss"));
-            vnpay.AddRequestData("vnp_CurrCode", "VND");
-            vnpay.AddRequestData("vnp_IpAddr", Utils.GetIpAddress());
-            vnpay.AddRequestData("vnp_Locale", "vn");
-            vnpay.AddRequestData("vnp_OrderInfo", "Thanh toán dơn hàng: " + order.Code);
-            vnpay.AddRequestData("vnp_OrderType", "other"); //default value: other
-
-            vnpay.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
-            vnpay.AddRequestData("vnp_TxnRef", order.Code); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
-
-            //Add Params of 2.1.0 Version
-            //Billing
-
-            string paymentUrl = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
-            //log.InfoFormat("VNPAY URL: {0}", paymentUrl);
-            //Response.Redirect(paymentUrl);
-            return urlPayment;
         }
     }
 }
