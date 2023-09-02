36ebd02395b21f2476aa12f49e492bac46c3cf84
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2023-09-02 09:26:38 +0700

    update database, comment những code dư thừa trên template

---
+++ WebBanHangOnline/WebBanHangOnline/Migrations/202309020211211_Update.cs
@@ -0,0 +1,22 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.tb_Subscribe", "Email", c => c.String());
+            AlterColumn("dbo.tb_SystemSetting", "SettingValue", c => c.String(maxLength: 500));
+            AlterColumn("dbo.tb_SystemSetting", "SettingDescription", c => c.String(maxLength: 500));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.tb_SystemSetting", "SettingDescription", c => c.String(maxLength: 4000));
+            AlterColumn("dbo.tb_SystemSetting", "SettingValue", c => c.String(maxLength: 4000));
+            AlterColumn("dbo.tb_Subscribe", "Email", c => c.String(nullable: false));
+        }
+    }
+}
