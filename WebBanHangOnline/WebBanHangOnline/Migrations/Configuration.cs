bc95dc7b8ab85df0cd7e5d0e4db038b20c8daaca
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2023-09-04 16:29:38 +0700

    update

--- WebBanHangOnline/WebBanHangOnline/Migrations/Configuration.cs
+++ WebBanHangOnline/WebBanHangOnline/Migrations/Configuration.cs
@@ -20,20 +20,7 @@
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
 
-            context.ProductCategories.AddOrUpdate(x => x.Id,
-                new ProductCategory() { Id = 1, Title = "Cà phê", Description = "Cà phê hạt", Icon="", CreateBy="Duy", CreateDate=DateTime.Parse("01-01-2023"), ModifierBy="", ModifierDate=DateTime.Now },
-                new ProductCategory() { Id = 2, Title = "Trà", Description = "Trà trái cây", Icon="", CreateBy = "Duy", CreateDate = DateTime.Parse("01-01-2023"), ModifierBy = "", ModifierDate = DateTime.Now },
-                new ProductCategory() { Id = 3, Title = "Sữa", Description = "Sữa tươi", Icon="", CreateBy = "Duy", CreateDate = DateTime.Parse("01-01-2023"), ModifierBy = "", ModifierDate = DateTime.Now }
-            );
-
-            context.Products.AddOrUpdate(x => x.Id,
-                new Product() { Id = 1, Title = "Cà phê đen không đường", ProductCode="", ProductCategoryID=1, Description = "Cà phê hạt", Detail="", Image="", Price=0, PriceSale=0, IsHome=true,IsSale=true, IsFeature = true, IsHot = true, Quantity=0, SeoTitle="", SeoKeywords="", SeoDescription="", CreateBy = "Duy", CreateDate = DateTime.Parse("01-01-2023"), ModifierBy = "", ModifierDate = DateTime.Now },
-                new Product() { Id = 2, Title = "Cà phê đen có đường", ProductCode = "", ProductCategoryID = 1, Description = "Cà phê hạt", Detail = "", Image = "", Price = 0, PriceSale = 0, IsHome = true, IsSale = true, IsFeature = true, IsHot = true, Quantity = 0, SeoTitle = "", SeoKeywords = "", SeoDescription = "", CreateBy = "Duy", CreateDate = DateTime.Parse("01-01-2023"), ModifierBy = "", ModifierDate = DateTime.Now },
-                new Product() { Id = 3, Title = "Cà phê đen đường đá", ProductCode = "", ProductCategoryID = 1, Description = "Cà phê hạt", Detail = "", Image = "", Price = 0, PriceSale = 0, IsHome = true, IsSale = true, IsFeature = true, IsHot = true, Quantity = 0, SeoTitle = "", SeoKeywords = "", SeoDescription = "", CreateBy = "Duy", CreateDate = DateTime.Parse("01-01-2023"), ModifierBy = "", ModifierDate = DateTime.Now },
-                new Product() { Id = 4, Title = "Cà phê đen nóng", ProductCode = "", ProductCategoryID = 1, Description = "Cà phê hạt", Detail = "", Image = "", Price = 0, PriceSale = 0, IsHome = true, IsSale = true, IsFeature = true, IsHot = true, Quantity = 0, SeoTitle = "", SeoKeywords = "", SeoDescription = "", CreateBy = "Duy", CreateDate = DateTime.Parse("01-01-2023"), ModifierBy = "", ModifierDate = DateTime.Now },
-                new Product() { Id = 5, Title = "Cà phê sữa", ProductCode = "", ProductCategoryID = 1, Description = "Cà phê hạt", Detail = "", Image = "", Price = 0, PriceSale = 0, IsHome = true, IsSale = true, IsFeature = true, IsHot = true, Quantity = 0, SeoTitle = "", SeoKeywords = "", SeoDescription = "", CreateBy = "Duy", CreateDate = DateTime.Parse("01-01-2023"), ModifierBy = "", ModifierDate = DateTime.Now }
-
-            );
+           
 
         }
     }
