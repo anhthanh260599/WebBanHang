250abad953ce7cdf929b202475d77c3165447606
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2023-09-05 23:17:22 +0700

    Apply phân quyền, thêm creatby các trang tin

--- WebBanHangOnline/WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
+++ WebBanHangOnline/WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
@@ -1,4 +1,6 @@
-﻿using PagedList;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +32,25 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                // Lấy thông tin người dùng hiện tại
+                var currentUserId = User.Identity.GetUserId(); // Sử dụng UserManager để lấy UserId của người dùng hiện tại
+
+                // Sử dụng DbContext để tìm ApplicationUser có UserId tương ứng
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                var currentUser = userManager.FindById(currentUserId);
+
+                if (currentUser != null)
+                {
+                    // Gán giá trị FullName của người dùng hiện tại cho CreateBy của đối tượng News
+                    model.CreateBy = currentUser.FullName;
+                }
+                else
+                {
+                    // Xử lý trường hợp không tìm thấy người dùng
+                    // Chẳng hạn, bạn có thể gán một giá trị mặc định hoặc xử lý khác
+                    model.CreateBy = "Người dùng không tồn tại"; // Ví dụ
+                }
+
                 model.CreateDate = DateTime.Now;
                 model.CategoryID = 18; // CategoryID của Tin tức (check trong DB)
                 model.ModifierDate = DateTime.Now;
