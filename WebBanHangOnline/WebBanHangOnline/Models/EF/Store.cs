b5dde591f7ee028385925b353e4e3149f7d73821
duythong2505@gmail.com <duythong2505@gmail.com>
2023-09-09 08:43:28 +0700

    test

---
+++ WebBanHangOnline/WebBanHangOnline/Models/EF/Store.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tb_Store")]
+    public class Store
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
