a281ecfaed40cfd0af6e6d13c565663d2ed7ffcf
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2023-09-14 20:29:04 +0700

    upcode quản lý footer

---
+++ WebBanHangOnline/WebBanHangOnline/Areas/Admin/Controllers/SocialMediaPluginController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class SocialMediaPluginController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/SocialMediaPlugin
+        public ActionResult Index()
+        {
+            var items = db.SocialMediaPlugins.ToList();
+            return View(items);
+        }
+
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(SocialMediaPlugin model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SocialMediaPlugins.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.SocialMediaPlugins.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(SocialMediaPlugin model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SocialMediaPlugins.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+
+            var item = db.SocialMediaPlugins.Find(id);
+            if (item != null)
+            {
+                db.SocialMediaPlugins.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult DeleteSelected(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = db.SocialMediaPlugins.Find(Convert.ToInt32(item));
+                        db.SocialMediaPlugins.Remove(obj);
+                        db.SaveChanges();
+                    }
+                }
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
---
+++ WebBanHangOnline/WebBanHangOnline/Controllers/FooterController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class FooterController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Footer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Partial_FooterSocialMedia()
+        {
+            var items = db.SocialMediaProfiles.ToList();
+            return PartialView(items);
+        }
+        public ActionResult Partial_FooterSocialMediaPlugin()
+        {
+            var items = db.SocialMediaPlugins.ToList();
+            return PartialView(items);
+        }
+
+        public ActionResult Partial_FooterContactInfo()
+        {
+            var items = db.ContactInfos.ToList();
+            return PartialView(items);
+        }
+
+    }
+}
