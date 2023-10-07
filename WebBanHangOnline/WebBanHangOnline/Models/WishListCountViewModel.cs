a0764fa81c3620f41e09b50317137f5216e16f5b
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2023-10-07 18:17:01 +0700

    up code trang chủ admin part 1, fix lỗi khi sử dụng mã khuyến mãi

---
+++ WebBanHangOnline/WebBanHangOnline/Models/WishListCountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models
+{
+    public class WishListCountViewModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public int WishCount { get; set; }
+    }
+}
