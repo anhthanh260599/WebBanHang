50c98fdaf8786619cbbfe674a4c9b0cc55eca413
duythong2505@gmail.com <duythong2505@gmail.com>
2023-11-02 18:26:57 +0700

    Revert "edit stored update Status. Hoàn thành chức năng Điều Phối Đơn Hàng"
    This reverts commit 9a5d5102e935425f9c960622c5aeac7517fa4138.

--- WebBanHangOnline/WebBanHangOnline/Models/Stored Procedure/UpdateStatusIfNoChange.sql
+++ WebBanHangOnline/WebBanHangOnline/Models/Stored Procedure/UpdateStatusIfNoChange.sql
@@ -22,12 +22,4 @@ BEGIN
     WHERE TypePayment = 3
     AND Status = 1
     AND DATEDIFF(MINUTE, ModifierDate, @CurrentTime) >= 15;
-
-    -- Xử lý cho TypePayment = 1 và IsConfirm = False // Nếu khách hàng không xác nhận đơn trong vòng 15p thì đơn hàng sẽ tự huỷ
-    UPDATE tb_Order
-    SET Status = 4
-    WHERE TypePayment = 1 
-    AND Status = 1
-	AND IsConfirm = 0
-    AND DATEDIFF(MINUTE, ModifierDate, @CurrentTime) >= 15;
 END;
