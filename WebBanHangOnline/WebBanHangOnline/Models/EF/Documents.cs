cf59f45cb70adeea34cdab0e2447bab988d72918
duythong2505@gmail.com <duythong2505@gmail.com>
2023-12-03 23:50:53 +0700

    Add Prototype Pattern

--- WebBanHangOnline/WebBanHangOnline/Models/EF/Documents.cs
+++ WebBanHangOnline/WebBanHangOnline/Models/EF/Documents.cs
@@ -4,17 +4,25 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Services.Description;
 
 namespace WebBanHangOnline.Models.EF
 {
+    public interface DocumentsPrototype
+    {
+        DocumentsPrototype Clone();
+    }
+
+
     [Table("tb_Documents")]
-    public class Documents : CommonAbstract
+    public class Documents : CommonAbstract, DocumentsPrototype
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // tự động tăng 
         public int Id { get; set; }
         public string Name { get; set; }
         public string File { get; set; }
+
         public override void SetCreated()
         {
             this.CreateDate = DateTime.Now;
@@ -37,6 +45,22 @@ namespace WebBanHangOnline.Models.EF
         public override void SetModifiedBy()
         {
 
+        }
+
+        public DocumentsPrototype Clone()
+        {
+            // Khong nhan ban ID
+
+            //Documents newDocument = new Documents();
+            //newDocument.Name = Name;
+            //newDocument.File = File;
+            //newDocument.CreateBy = CreateBy;
+            //newDocument.ModifierBy = ModifierBy;
+            //newDocument.ModifierDate = ModifierDate;
+            //newDocument.CreateDate = CreateDate;
+            //return newDocument;
+
+            return (DocumentsPrototype)this.MemberwiseClone();
         }
     }
 }
