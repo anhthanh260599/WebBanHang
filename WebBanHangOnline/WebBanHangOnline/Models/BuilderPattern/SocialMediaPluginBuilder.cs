1767fd89cd23ab27712ecf005ce4692b0f2f4ea3
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2023-12-06 17:39:16 +0700

    upcode Builder Pattern

---
+++ WebBanHangOnline/WebBanHangOnline/Models/BuilderPattern/SocialMediaPluginBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Models.BuilderPattern
+{
+    public class SocialMediaPluginBuilder
+    {
+        private SocialMediaPlugin _socialMediaPlugin = new SocialMediaPlugin();
+
+        public SocialMediaPluginBuilder WithName(string name) 
+        {
+            _socialMediaPlugin.Name = name;
+            return this;
+        }
+
+        public SocialMediaPluginBuilder WithPlugin(string plugin)
+        {
+            _socialMediaPlugin.Plugin = plugin;
+            return this;
+        }
+
+        public SocialMediaPluginBuilder WithImage (string image)
+        {
+            _socialMediaPlugin.Image = image;
+            return this;
+        }
+
+        public SocialMediaPlugin Build()
+        {
+            return _socialMediaPlugin;
+        }
+    }
+}
