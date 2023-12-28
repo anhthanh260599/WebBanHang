176600d7e0ad7921a96d00ec7bd9afd3a577cb49
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2023-12-28 17:17:29 +0700

    upcode quy đổi tiền tệ bằng API

---
+++ WebBanHangOnline/WebBanHangOnline/Models/ExchangeCurrency/ExchangeRate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.ExchangeCurrency
+{
+
+    // Sử dụng API: "https://open.er-api.com/v6/latest/USD"
+    // Lưu ý: Khai báo fields nhớ giống với các Key ở link API
+    public class ExchangeRate
+    {
+        public string Result { get; set; }
+        public string Provider { get; set; }
+        public string Documentation { get; set; }
+        public string Terms_Of_Use { get; set; }
+        public long Time_Last_Update_Unix { get; set; }
+        public DateTime Time_Last_Update_Utc { get; set; }
+        public long Time_Next_Update_Unix { get; set; }
+        public DateTime Time_Next_Update_Utc { get; set; }
+        public long Time_Eol_Unix { get; set; }
+        public string Base_Code { get; set; }
+        public Rates Rates { get; set; }
+    }
+}
---
+++ WebBanHangOnline/WebBanHangOnline/Models/ExchangeCurrency/Rates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.ExchangeCurrency
+{
+    public class Rates
+    {
+        public double USD { get; set; }
+        public double VND { get; set; }
+        // Thêm các đơn vị tiền tệ khác nếu cần thiết
+    }
+}
